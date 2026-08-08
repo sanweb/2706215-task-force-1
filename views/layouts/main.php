392df7e0cac564dec574214725bad64d788e7874
San <sanwebdev@yandex.ru>
2026-08-08 18:13:49 +0300

    [module4-task1] Move assets to web dir and update main layout and site view

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,51 +21,70 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
+<html lang="<?= Yii::$app->language ?>">
 <head>
+    <!-- <meta charset="utf-8"> -->
+    <!-- <meta name="viewport" content="width=device-width,initial-scale=1"> -->
+    <!-- <title>Taskforce</title> -->
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body class="d-flex flex-column h-100">
+<body>
 <?php $this->beginBody() ?>
 
-<header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest
-                ? ['label' => 'Login', 'url' => ['/site/login']]
-                : '<li class="nav-item">'
-                    . Html::beginForm(['/site/logout'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
-                        ['class' => 'nav-link btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>'
-        ]
-    ]);
-    NavBar::end();
-    ?>
+<header class="page-header">
+    <nav class="main-nav">
+        <a href='#' class="header-logo">
+            <img class="logo-image" src="img/logotype.png" width=227 height=60 alt="taskforce">
+        </a>
+        <div class="nav-wrapper">
+            <ul class="nav-list">
+                <li class="list-item list-item--active">
+                    <a class="link link--nav" >Новое</a>
+                </li>
+                <li class="list-item">
+                    <a href="#" class="link link--nav" >Мои задания</a>
+                </li>
+                <li class="list-item">
+                    <a href="#" class="link link--nav" >Создать задание</a>
+                </li>
+                <li class="list-item">
+                    <a href="#" class="link link--nav" >Настройки</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="user-block">
+        <a href="#">
+            <img class="user-photo" src="img/man-glasses.png" width="55" height="55" alt="Аватар">
+        </a>
+        <div class="user-menu">
+            <p class="user-name">Василий</p>
+            <div class="popup-head">
+                <ul class="popup-menu">
+                    <li class="menu-item">
+                        <a href="#" class="link">Настройки</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#" class="link">Связаться с нами</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#" class="link">Выход из системы</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
 </header>
 
-<main id="main" class="flex-shrink-0" role="main">
-    <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
+<main class="main-content container">
+    <?php if (!empty($this->params['breadcrumbs'])): ?>
+        <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
+    <?php endif ?>
+    <?= Alert::widget() ?>
+    <?= $content ?>
 </main>
 
 <footer id="footer" class="mt-auto py-3 bg-light">
