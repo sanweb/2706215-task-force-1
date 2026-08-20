8e65ce6850c1cd744b94f6228cf7ff62ee39bc73
San <sanwebdev@yandex.ru>
2026-08-20 23:31:32 +0300

    [module5-task1] Update LoginForm model

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -11,31 +11,34 @@ use yii\base\Security;
 /**
  * LoginForm is the model behind the login form.
  *
- * @property-read User|null $user
+ * @property-read ?User $user
  *
+ */
 class LoginForm extends Model
 {
-    public string $username = '';
+    public string $email = '';
     public string $password = '';
     public bool $rememberMe = true;
-    private User|null $_user = null;
+
+    private ?User $_user = null;
     private bool $_userLoaded = false;
-    public function __construct(private readonly Security $security, $config = [])
-    {
+
+    public function __construct(
+        private readonly Security $security,
+        $config = []
+    ) {
         parent::__construct($config);
     }
 
     /**
-     * @return array the validation rules.
+     * Returns the validation rules.
      */
     public function rules(): array
     {
         return [
-            // username and password are both required
-            [['username', 'password'], 'required'],
-            // rememberMe must be a boolean value
+            [['email', 'password'], 'required'],
+            ['email', 'email'],
             ['rememberMe', 'boolean'],
-            // password is validated by validatePassword()
             ['password', 'validatePassword'],
         ];
     }
@@ -45,22 +48,25 @@ class LoginForm extends Model
      * This method serves as the inline validation for password.
      *
      * @param string $attribute the attribute currently being validated
-     * @param array $params the additional name-value pairs given in the rule
+     * @param ?array $params the additional name-value pairs given in the rule
      */
-    public function validatePassword(string $attribute, array|null $params): void
+    public function validatePassword(string $attribute, ?array $params): void
     {
         if (!$this->hasErrors()) {
             $user = $this->getUser();
 
-            if (!$user || !$this->security->validatePassword($this->password, $user->passwordHash)) {
-                $this->addError($attribute, 'Incorrect username or password.');
+            if (
+                $user === null
+                || $user->password === null
+                || !$this->security->validatePassword($this->password, $user->password)
+            ) {
+                $this->addError($attribute, 'Incorrect email or password.');
             }
         }
     }
 
     /**
-     * Logs in a user using the provided username and password.
-     * @return bool whether the user is logged in successfully
+     * Logs in a user using the provided email and password.
      */
     public function login(): bool
     {
@@ -72,14 +78,12 @@ class LoginForm extends Model
     }
 
     /**
-     * Finds user by [[username]]
-     *
-     * @return User|null
+     * Finds user by email.
      */
-    public function getUser(): User|null
+    public function getUser(): ?User
     {
         if (!$this->_userLoaded) {
-            $this->_user = User::findByUsername($this->username);
+            $this->_user = User::findByEmail($this->email);
             $this->_userLoaded = true;
         }
 
