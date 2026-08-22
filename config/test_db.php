8336229f2b99a78a272387ff6185939ea719fc56
San <sanwebdev@yandex.ru>
2026-08-22 18:57:03 +0300

    [module5-task1] Update test_db config

--- config/test_db.php
+++ config/test_db.php
@@ -2,6 +2,11 @@
 
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2basic_test';
+$db['dsn'] = sprintf(
+    'mysql:host=%s;port=%s;dbname=%s',
+    $_ENV['DB_HOST'],
+    $_ENV['DB_PORT'],
+    'taskforce_yii2_test',
+);
 
 return $db;
