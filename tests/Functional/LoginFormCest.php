95d9954d7a3ee73922b2ae1b49d7c2a63b94d505
San <sanwebdev@yandex.ru>
2026-08-22 18:56:34 +0300

    [module5-task1] Fix LoginFormCest.php

--- tests/Functional/LoginFormCest.php
+++ tests/Functional/LoginFormCest.php
@@ -4,61 +4,70 @@ declare(strict_types=1);
 
 namespace app\tests\Functional;
 
+use app\models\User;
+use app\tests\fixtures\UserFixture;
 use app\tests\Support\FunctionalTester;
 
 final class LoginFormCest
 {
-    public function _before(FunctionalTester $I)
+    public function _fixtures(): array
+    {
+        return [
+            'users' => UserFixture::class,
+        ];
+    }
+
+    public function _before(FunctionalTester $I): void
     {
         $I->amOnRoute('site/login');
     }
 
-    public function openLoginPage(FunctionalTester $I)
+    public function openLoginPage(FunctionalTester $I): void
     {
         $I->see('Login', 'h1');
     }
 
     // demonstrates `amLoggedInAs` method
-    public function internalLoginById(FunctionalTester $I)
+    public function internalLoginById(FunctionalTester $I): void
     {
         $I->amLoggedInAs(100);
         $I->amOnPage('/');
-        $I->see('Logout (admin)');
+        $I->see('Logout (Admin)');
     }
 
     // demonstrates `amLoggedInAs` method
-    public function internalLoginByInstance(FunctionalTester $I)
+    public function internalLoginByInstance(FunctionalTester $I): void
     {
-        $I->amLoggedInAs(\app\models\User::findByUsername('admin'));
+        $I->amLoggedInAs(User::findByEmail('admin@example.com'));
         $I->amOnPage('/');
-        $I->see('Logout (admin)');
+        $I->see('Logout (Admin)');
     }
 
-    public function loginWithEmptyCredentials(FunctionalTester $I)
+    public function loginWithEmptyCredentials(FunctionalTester $I): void
     {
         $I->submitForm('#login-form', []);
-        $I->expectTo('see validations errors');
-        $I->see('Username cannot be blank.');
+        $I->expectTo('see validation errors');
+        $I->see('Email cannot be blank.');
         $I->see('Password cannot be blank.');
     }
 
-    public function loginWithWrongCredentials(FunctionalTester $I)
+    public function loginWithWrongCredentials(FunctionalTester $I): void
     {
         $I->submitForm('#login-form', [
-            'LoginForm[username]' => 'admin',
+            'LoginForm[email]' => 'admin@example.com',
             'LoginForm[password]' => 'wrong',
         ]);
-        $I->expectTo('see validations errors');
-        $I->see('Incorrect username or password.');
+        $I->expectTo('see validation errors');
+        $I->see('Incorrect email or password.');
     }
 
-    public function loginSuccessfully(FunctionalTester $I)
+    public function loginSuccessfully(FunctionalTester $I): void
     {
         $I->submitForm('#login-form', [
-            'LoginForm[username]' => 'admin',
+            'LoginForm[email]' => 'admin@example.com',
             'LoginForm[password]' => 'admin',
         ]);
-        $I->see('Logout (admin)');
+        $I->see('Logout (Admin)');
         $I->dontSeeElement('form#login-form');
     }
 }
