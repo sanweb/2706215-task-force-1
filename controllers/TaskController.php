99182600a7c153bcbf5fd96675390edd72653e03
San <sanwebdev@yandex.ru>
2026-08-28 19:45:47 +0300

    [module6-task1] Add task filter form

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -4,20 +4,53 @@ declare(strict_types=1);
 
 namespace app\controllers;
 
+use app\models\Category;
 use app\models\Task;
+use app\requests\TaskFilterRequest;
 use Sanweb\Taskforce\enum\TaskStatus;
+use Yii;
 use yii\web\Controller;
 
 class TaskController extends Controller
 {
     public function actionIndex(): string
     {
-        $tasks = Task::find()
-            ->where(['status' => TaskStatus::New->value])
-            ->orderBy(['created_at' => SORT_DESC])
-            ->with(['category', 'city'])
-            ->all();
+        $filterForm = new TaskFilterRequest();
+        $filterForm->load(Yii::$app->request->queryParams);
 
-        return $this->render('index', ['tasks' => $tasks]);
+        $query = Task::find()
+            ->where(['task.status' => TaskStatus::New->value])
+            ->orderBy(['task.created_at' => SORT_DESC])
+            ->with(['category', 'city']);
+
+        if ($filterForm->validate()) {
+            $query->andFilterWhere(['in', 'category_id', $filterForm->categories]);
+
+            if ($filterForm->isRemote) {
+                $query->andWhere(['city_id' => null]);
+            }
+
+            if ($filterForm->hasNoBid) {
+                $query->joinWith('bids', false)
+                    ->andWhere(['bid.id' => null]);
+            }
+
+            if ($filterForm->period !== '') {
+                $timestamp = strtotime($filterForm->period);
+
+                if ($timestamp !== false) {
+                    $query->andWhere(['>=', 'task.created_at', $timestamp]);
+                }
+            }
+        }
+
+        $tasks = $query->all();
+        $categories = Category::find()->all();
+
+        return $this->render('index', [
+            'tasks' => $tasks,
+            'categories' => $categories,
+            'filterForm' => $filterForm
+        ]);
     }
 }
