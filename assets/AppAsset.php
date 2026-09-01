c1f639ecdc6849bcf2285e099897bb2f2f8055a3
San <sanwebdev@yandex.ru>
2026-09-01 18:59:11 +0300

    [module6-task2] Actualize AppAsset and remove temporary comments from layout

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -1,25 +1,14 @@
 <?php
 
-/**
- * @link https://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license https://www.yiiframework.com/license/
- */
-
 declare(strict_types=1);
 
 namespace app\assets;
 
-use yii\bootstrap5\BootstrapAsset;
 use yii\web\AssetBundle;
 use yii\web\View;
-use yii\web\YiiAsset;
 
 /**
  * Main application asset bundle.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
  */
 class AppAsset extends AssetBundle
 {
@@ -28,14 +17,6 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/style.css',
     ];
-    public $js = [
-        //'js/color-mode.js',
-    ];
-    public $jsOptions = [
-        'position' => View::POS_HEAD,
-    ];
-    public $depends = [
-        //YiiAsset::class,
-        //BootstrapAsset::class,
-    ];
+    public $js = [];
+    public $depends = [];
 }
